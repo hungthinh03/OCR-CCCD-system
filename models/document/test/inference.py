b6bd4778858f98a23917d4e09a4af4299d16c516
Nguyễn Đức Hưng Thịnh <nguyenduchungthinh03@gmail.com>
2026-05-12 16:24:16 +0700

    Added tests

--- models/document/test/inference.py
+++ models/document/test/inference.py
@@ -2,13 +2,14 @@ import cv2
 from ultralytics import YOLO
 
 model = YOLO("models/document/best.pt")
-results = model("models/document/test.jpg")
 
-img = cv2.imread("models/document/test.jpg")
+results = model("models/document/test/test.jpg")
+
+img = cv2.imread("models/document/test/test.jpg")
 
 for box in results[0].boxes.xyxy:
     x1, y1, x2, y2 = map(int, box)
     cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 10)
 
-cv2.imwrite("models/document/output.jpg", img)
+cv2.imwrite("models/document/test/output.jpg", img)
 
---
+++ models/field/test/inference.py
@@ -0,0 +1,49 @@
+import cv2
+from ultralytics import YOLO
+
+# Load model
+model = YOLO("models/field/best.pt")
+
+CLASSES = [
+    "id",
+    "name",
+    "dob",
+    "gender",
+    "nationality",
+    "hometown",
+    "address",
+    "expiry"
+]
+
+# Predict
+results = model("models/field/test/test.jpg")
+
+# Read image
+img = cv2.imread("models/field/test/test.jpg")
+
+# Get boxes + classes
+boxes = results[0].boxes.xyxy.cpu().numpy()
+classes = results[0].boxes.cls.cpu().numpy()
+
+# Draw predictions
+for box, cls_id in zip(boxes, classes):
+    x1, y1, x2, y2 = map(int, box)
+
+    field_name = CLASSES[int(cls_id)]
+
+    # Draw rectangle
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+    # Draw label
+    cv2.putText(
+        img,
+        field_name,
+        (x1, y1 - 10),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+# Save output
+cv2.imwrite("models/field/test/output.jpg", img)
